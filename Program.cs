fce71b13970f152761476c8e581631f8c2c91ef5
2014000044IlanMotta <i.motta.5bdb@gmail.com>
2026-03-24 23:25:59 -0600

    Add files via upload
    Visual ejercicio NumPosNeg

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        {
+            int a;
+            Console.WriteLine("===NUMEROS POSITIVOS Y NEGATIVOS===");
+            Console.WriteLine("Escribe un numero entero: ");
+            a = int.Parse(Console.ReadLine());
+            if (a >= 0)
+            {
+                Console.WriteLine("Numero Positivo ");
+            }
+            else
+            {
+                Console.WriteLine("Numero Negativo ");
+            }
+
+        }
+    }
+}
